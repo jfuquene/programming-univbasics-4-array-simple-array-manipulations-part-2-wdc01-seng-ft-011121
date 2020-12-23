c36a294958f8331fce337251abdc1336a4644124
John Fuquene <jfuquene@email.wm.edu>
2020-12-23 19:05:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,9 @@ end
 def using_flatten(array)
   array.flatten 
 end 
+def using_delete(array, string)
+  array.delete(string)
+end 
 def using_delete_at(array, Integer)
   array.delete_at(Integer)
 end 
